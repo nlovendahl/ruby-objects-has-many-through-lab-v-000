af6a4f9bd499b02b31f044517b30ce68e60b9754
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-23 19:02:41 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -7,6 +7,7 @@ class Doctor
   def initialize(name)
     @name = name
     @@all << self
+    @appointments = []
   end
 
   def self.all
@@ -16,5 +17,8 @@ class Doctor
   def new_appointment(patient, date)
     appointment = Appointment.new(patient, date, self)
   end
+
+  def appointments
+
 
 end
